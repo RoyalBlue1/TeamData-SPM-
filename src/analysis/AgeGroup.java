729f10da53633631a399524a01e3e3d554a13272
alex- <alex-@DESKTOP-2GE0K5B.fritz.box>
2018-05-31 07:59:54 +0200

    test

--- src/analysis/AgeGroup.java
+++ src/analysis/AgeGroup.java
@@ -5,7 +5,7 @@ package analysis;
  * @author alex-
  * @version 1
  * 
- * Klasse die die Altergruppen unterteilt
+ * Klasse die die Altersgruppen unterteilt
  */
 
 public class AgeGroup implements Comparable<AgeGroup>{
